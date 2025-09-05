9629ae68fa37ebdfd0a7648e7fd8195bd2c2038a
anybody <anybodyy69@gmail.com>
2025-09-05 18:08:35 +0700

    Fix canvas importing

--- quillion/components/ui/__init__.py
+++ quillion/components/ui/__init__.py
@@ -1,6 +1,6 @@
 from .element import Element
 from .text import text
 from .button import button
-from .canvas import Canvas
+from .canvas import canvas
 from .container import container
 from .image import image
