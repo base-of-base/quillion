3f5e76991c7d086d3d92ec63b137b83cbec7e3d3
anybody <anybodyy69@gmail.com>
2025-09-06 10:13:47 +0700

    Add params validation

--- quillion/utils/decorators.py
+++ quillion/utils/decorators.py
@@ -1,12 +1,15 @@
-from typing import Callable, Union
+from typing import Callable, Union, Any, Dict, get_type_hints
 import inspect
 import re
+from pydantic import validate_arguments, ValidationError
 
 
 def page(route: Union[str, re.Pattern], priority: int = 0):
     from ..pages.base import Page, PageMeta
 
     def decorator(func: Callable):
+        validated = validate_arguments(func)
+
         class GeneratedPage(Page, metaclass=PageMeta):
             original_router = route
             _priority = priority
@@ -15,12 +18,12 @@ def page(route: Union[str, re.Pattern], priority: int = 0):
             if inspect.iscoroutinefunction(func):
 
                 async def render(self, **params):
-                    return await func(**params)
+                    return await validated(**params)
 
             else:
 
                 def render(self, **params):
-                    return func(**params)
+                    return validated(**params)
 
         GeneratedPage.__name__ = func.__name__
         return GeneratedPage
