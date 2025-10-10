beda3982e0a3ed395d25556555e24ec742588731
anybody <anybodyy69@gmail.com>
2025-10-10 21:17:43 +0700

    Add more tags

--- quillion/components/ui/__init__.py
+++ quillion/components/ui/__init__.py
@@ -1,7 +1,167 @@
 from .element import Element, MediaElement
-from .base.text import text
+
+from .element import StyleProperty
+from .element import Element
+from .element import MediaElement
+from .base.anchor import Anchor
+from .base.anchor import anchor
+from .base.article import Article
+from .base.article import article
+from .base.aside import Aside
+from .base.aside import aside
+from .base.button import Button
 from .base.button import button
-from .media.canvas import canvas
+from .base.container import Container
 from .base.container import container
-from .media.image import image
+from .base.footer import Footer
+from .base.footer import footer
+from .base.form import Form
+from .base.form import form
+from .base.heading import Heading
+from .base.heading import heading
+from .base.input import Input
 from .base.input import input
+from .base.label import Label
+from .base.label import label
+from .base.list_item import ListItem
+from .base.list_item import list_item
+from .base.main import Main
+from .base.main import main
+from .base.navigation import Navigation
+from .base.navigation import navigation
+from .base.option import Option
+from .base.option import option
+from .base.ordered_list import OrderedList
+from .base.ordered_list import ordered_list
+from .base.paragraph import Paragraph
+from .base.paragraph import paragraph
+from .base.section import Section
+from .base.section import section
+from .base.select import Select
+from .base.select import select
+from .base.span import Span
+from .base.span import span
+from .base.table import Table
+from .base.table import table
+from .base.table_cell import TableCell
+from .base.table_cell import table_cell
+from .base.table_header import TableHeader
+from .base.table_header import table_header
+from .base.table_row import TableRow
+from .base.table_row import table_row
+from .base.text import Text
+from .base.text import text
+from .base.textarea import TextArea
+from .base.textarea import textarea
+from .base.unordered_list import UnorderedList
+from .base.unordered_list import unordered_list
+from .media.area import Area
+from .media.area import area
+from .media.audio import Audio
+from .media.audio import audio
+from .media.canvas import Canvas
+from .media.canvas import canvas
+from .media.embed import Embed
+from .media.embed import embed
+from .media.figure import Figure
+from .media.figure import figure
+from .media.fig_caption import FigCaption
+from .media.fig_caption import figcaption
+from .media.iframe import Iframe
+from .media.iframe import iframe
+from .media.image import Image
+from .media.image import image
+from .media.map import Map
+from .media.map import map
+from .media.object import Object
+from .media.object import object
+from .media.source import Picture
+from .media.source import picture
+from .media.svg import Svg
+from .media.svg import svg
+from .media.track import Track
+from .media.track import track
+
+__all__ = [
+    'Anchor',
+    'Area',
+    'Article',
+    'Aside',
+    'Audio',
+    'Button',
+    'Canvas',
+    'Container',
+    'Element',
+    'Embed',
+    'FigCaption',
+    'Figure',
+    'Footer',
+    'Form',
+    'Heading',
+    'Iframe',
+    'Image',
+    'Input',
+    'Label',
+    'ListItem',
+    'Main',
+    'Map',
+    'MediaElement',
+    'Navigation',
+    'Object',
+    'Option',
+    'OrderedList',
+    'Paragraph',
+    'Picture',
+    'Section',
+    'Select',
+    'Span',
+    'StyleProperty',
+    'Svg',
+    'Table',
+    'TableCell',
+    'TableHeader',
+    'TableRow',
+    'Text',
+    'TextArea',
+    'Track',
+    'UnorderedList',
+    'anchor',
+    'area',
+    'article',
+    'aside',
+    'audio',
+    'button',
+    'canvas',
+    'container',
+    'embed',
+    'figcaption',
+    'figure',
+    'footer',
+    'form',
+    'heading',
+    'iframe',
+    'image',
+    'input',
+    'label',
+    'list_item',
+    'main',
+    'map',
+    'navigation',
+    'object',
+    'option',
+    'ordered_list',
+    'paragraph',
+    'picture',
+    'section',
+    'select',
+    'span',
+    'svg',
+    'table',
+    'table_cell',
+    'table_header',
+    'table_row',
+    'text',
+    'textarea',
+    'track',
+    'unordered_list',
+]
